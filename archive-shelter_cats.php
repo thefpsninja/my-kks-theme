ec746623cc21241afcba842da4426923dd9f79e8
Sylvester <sylvester.lindfors@gmail.com>
2020-05-15 10:18:48 +0200

    Added shelter Cats archive

---
+++ archive-shelter_cats.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<div class="wrapper" id="archive-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<!-- Do the left sidebar check -->
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+
+			<main class="site-main" id="main">
+
+				<?php
+				if ( have_posts() ) {
+					?>
+					<header class="page-header">
+						<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+						?>
+					</header><!-- .page-header -->
+					<?php
+					// Start the loop.
+					while ( have_posts() ) {
+						the_post();
+
+						get_template_part( 'loop-templates/content', 'shelter_cats' );
+					}
+				} else {
+					get_template_part( 'loop-templates/content', 'none' );
+				}
+				?>
+
+			</main><!-- #main -->
+
+			<?php
+			// Display the pagination component.
+			understrap_pagination();
+			// Do the right sidebar check.
+			get_template_part( 'global-templates/right-sidebar-check' );
+			?>
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #archive-wrapper -->
+
+<?php
+get_footer();
