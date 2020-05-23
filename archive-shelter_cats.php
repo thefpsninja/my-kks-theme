ec03aded39ee547ce6b8af5d70f5498e40f3df30
Sylvester <sylvester.lindfors@gmail.com>
2020-05-23 13:58:22 +0200

    Fixed Sidebar

--- archive-shelter_cats.php
+++ archive-shelter_cats.php
@@ -22,6 +22,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 			<!-- Do the left sidebar check -->
 			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+			
 
 			<main class="site-main" id="main">
 
